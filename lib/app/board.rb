06ffb3ecfed9a2ffdc36fb2793fdd985c8e8eef0
enzostk <enzostk@gmail.com>
2022-02-03 23:07:29 +0100

    last commit

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -8,16 +8,22 @@ class Board
     end
     
     def display
-        puts " #{@tab[0]}  | #{@tab[1]} | #{@tab[2]} "
-        puts "-----------"
-        puts " #{@tab[3]}  | #{@tab[4]} | #{@tab[5]} "
-        puts "-----------"
-        puts " #{@tab[6]}  | #{@tab[7]} | #{@tab[8]} "
+        puts "                                                                       -------------"
+        puts "                                                                       | #{@tab[0]} | #{@tab[1]} | #{@tab[2]} |"
+        puts "                                                                       -------------"
+        puts "                                                                       | #{@tab[3]} | #{@tab[4]} | #{@tab[5]} |"
+        puts "                                                                       -------------"
+        puts "                                                                       | #{@tab[6]} | #{@tab[7]} | #{@tab[8]} |"
+        puts "                                                                       --------------"
     end
 
     def case_choice(symbol)
         case_number = gets.chomp.to_i
         @tab = @tab.each_index.map { |i| i == case_number && @tab[i] != "X" && @tab[i] != "O" ? @tab[i] = symbol : @tab[i] }
+    end
+
+    def draw?
+        puts "Il y a égalité entre les deux joueurs."
     end
 
     def victory?
--- lib/app/game.rb
+++ lib/app/game.rb
@@ -13,27 +13,41 @@ class Game
     
     def initialize
         system "clear"
-        puts "                  --------------------------------------------------------"
-        puts "                  |                                                      |"
-        puts "                  |           Bienvenue dans le jeu du Morpion !         |"
-        puts "                  |    Le but du jeu est de créer le premier alignment   |"
-        puts "                  |            dans un jeu sur grille de 3x3.            |"
-        puts "                  |  Le jeu s'arrête si toutes les cases sont remplies.  |"
-        puts "                  |                GOOD LUCK HAVE FUN !                  |"
-        puts "                  |         Si tu choisis une case occupée,              |"
-        puts "                  |                tu perds ton tour !                   |"
-        puts "                  |                                                      |"
-        puts "                  --------------------------------------------------------"
+        puts "                                                   --------------------------------------------------------"
+        puts "                                                   |                                                      |"
+        puts "                                                   |                                                      |"
+        puts "                                                   |           Bienvenue dans le jeu du Morpion !         |"
+        puts "                                                   |    Le but du jeu est de créer le premier alignment   |"
+        puts "                                                   |            dans un jeu sur grille de 3x3.            |"
+        puts "                                                   |  Le jeu s'arrête si toutes les cases sont remplies.  |"
+        puts "                                                   |  Choisir une case occupée te fait perdre ton tour.   |"
+        puts "                                                   |                                                      |"
+        puts "                                                   |                                                      |"
+        puts "                                                   |                       \\\\\||||||///                    |"
+        puts "                                                   |                        \\  ~ ~  /                     |"
+        puts "                                                   |                        (  @ @  )                     |"
+        puts "                                                   |               ______ oOOo-(_)-oOOo___________        |"
+        puts "                                                   |               .......                                |"            
+        puts "                                                   |               .......  GOOD LUCK HAVE FUN !          |"        
+        puts "                                                   |               .......                                |"                  
+        puts "                                                   |               _____________Oooo._____________        |"                    
+        puts "                                                   |                 .oooO     (   )                      |"              
+        puts "                                                   |                  (   )     )  /                      |"            
+        puts "                                                   |                   l  (     (_/                       |"                      
+        puts "                                                   |                    l _)                              |"            
+        puts "                                                   |                                                      |"
+        puts "                                                   |                                                      |"
+        puts "                                                   --------------------------------------------------------"
         puts ""
         # <------------------- Création Player 1 ------------------->
-        puts "Quel est le nom du premier joueur? (X)"
-        print "> "
+        puts "                                                   Quel est le nom du premier joueur? (Qui jouera avec les X)"
+        print "                                                                    >  "
         name1 = gets.chomp
         @player1 = Player.new(name1,"X")
         puts ""
         # <------------------- Création Player 2 ------------------->
-        puts "Quel est le nom du second joueur? (O)"
-        print "> "
+        puts "                                                   Quel est le nom du second joueur? (Qui jouera avec les O)"
+        print "                                                                   >  "
         name2 = gets.chomp
         @player2 = Player.new(name2,"O")
         puts ""
@@ -43,8 +57,8 @@ class Game
     end    
 
 #################################################################################################################################################
-  def go
-    # lance la partie
+
+  def go # Lancement de la partie 
     while @tab.victory? == false
       self.turn
     end
@@ -54,46 +68,47 @@ class Game
   def switch_players
     if @current_player == @player1
        @current_player = @player2
-       #@current_player.name = name2
-       #@current_player.symbol = @player2.symbol
     else
        @current_player = @player1
-       #@current_player.name = name1
-       #@current_player.symbol = @player1.symbol
     end
   end
 
-  #à chaque tour on exécute turn (lancé par la méthode go)
+#################################################################################################################################################
+
   def turn
-    # On boucle tant qu'il n'y a pas de victoire
-    loop do
-    	     
-      puts "=================="
-    	puts "Voici l'état du jeu:"
-    	# Affiche le plateau :
-      @tab.display
-
-      puts ""
-      puts "C'est le tour de #{@current_player.name} avec les #{@current_player.symbol}"
-      puts "Choisis une case"
-      print ">"
-
-      # On appelle la méthode play de la classe board sur le joueur en cours (current). Elle demande au joueur quelle case il joue, puis affiche son symbole dans la case
-      @tab.case_choice(@current_player.symbol)
-
-      # On arrête la boucle en cas de victoire
-      if @tab.victory? == true
-        @tab.display
+      10.times do |i|  # Boucle tant qu'il n'y a pas de victoire ou d'égalité   
+        puts "                                                          =========================================="
+        puts "                                                          |     Voici l'état du plateau de jeu     |"
+        puts "                                                          =========================================="
         puts ""
-        puts "#{@current_player.name} a gagné !!!"
-        puts
-        break
+        # Affichage du plateau :
+        puts "#{@tab.display}"
+
+        puts ""
+        puts "                                                          C'est le tour de #{@current_player.name} avec les #{@current_player.symbol}"
+        puts "                                                          Choisis une case"
+        print "                                                         >  "
+
+        # On appelle la méthode play de la classe board sur le joueur en cours (current). Elle demande au joueur quelle case il joue, puis affiche son symbole dans la case
+        @tab.case_choice(@current_player.symbol)
+        puts ""
+        # Arrêt de la boucle pour .victory?
+          if @tab.victory? == true
+            @tab.display
+            puts ""
+            puts "                                                        #{@current_player.name} a gagné !!!"
+            puts
+            break
+          end
+        switch_players
+        # Il n'y a pas de victoire : on passe au joueur suivant et on boucle (tour suivant)  
       end
-
-      # Il n'y a pas de victoire : on passe au joueur suivant et on boucle (tour suivant)
-      switch_players
-
-    end
   end 
 
+  def regame
+    puts "Voulez-vous relancer une partie?"
+  end
 end
+
+#################################################################################################################################################
+#################################################################################################################################################
